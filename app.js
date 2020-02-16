74b5f9bd3532c1006d1c376ed28342e3aa91363f
Lukas <lukas@lukasklinger.com>
2020-02-16 14:21:21 +0100

    app will retry on failed connection

--- app.js
+++ app.js
@@ -19,18 +19,40 @@ app.set('view engine', 'pug');
 app.set('views', './public/pug');
 
 app.get('/', async function(req, res){
-  let calls = await fritz.getCalls(options);
-  let data = new Object();
-
-  if (calls.error) {
-    console.log('Error: ' + calls.error.message);
-    res.send("Error: " + calls.error.message);
-    res.end();
-  } else {
-    data.calls = calls.slice(0, (parseInt(numberOfCalls)));
+  try {
+    let data = await updateCallList();
     res.render('index', data);
+  } catch (e) {
+    res.send("Error: " + e);
+    res.end();
   }
 });
+
+async function updateCallList() {
+  return new Promise(async function(resolve, reject) {
+    let retryCount = 0;
+    let finished = false;
+    let lastError;
+
+    while (retryCount < 3 && !finished) {
+      try {
+        let calls = await fritz.getCalls(options);
+        let data = new Object();
+
+        data.calls = calls.slice(0, (parseInt(numberOfCalls)));
+
+        finished = true;
+        console.log("Retries: " + retryCount);
+        resolve(data);
+      } catch (e) {
+        lastError = e;
+        retryCount++;
+      }
+    }
+
+    reject(lastError);
+  });
+}
 
 server.listen(3000);
 console.log("Listening on port 3000.");
